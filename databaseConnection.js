ef968067c7847fd3e0840dd8f19d48bc3b64acbf
Shivam Pandey <shivam.pandey@causecode.com>
2018-07-25 16:42:31 +0530

    connection established with mongodb

---
+++ databaseConnection.js
@@ -0,0 +1,12 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/MongoDatabase";
+
+MongoClient.connect(url, function(err, db){
+    if(err) {
+        throw err;
+    }
+
+    console.log('Hi Express, Welcome to MongoDB');
+
+    db.close();
+});
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const database = require('./databaseConnection');
+
+const os = require('os');
     app = express();
 
 app.get('/', (request, response)=>{
     response.send('Hello express.js');
+    console.warn('\x1b[36m%s\x1b[0m','this is a warning');
+    console.error(os.arch());
 });
 
 app.listen(3000, ()=>console.log('express server has been started at port 3000'));
