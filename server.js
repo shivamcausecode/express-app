48eac24814b4a4c0426464adf8bdff00e6c579c9
Shivam Pandey <shivam.pandey@causecode.com>
2018-07-27 16:38:13 +0530

    conclicts resolved

--- server.js
+++ server.js
@@ -5,4 +5,43 @@ app.get('/', (request, response)=>{
     response.send('Hello express.js');
 });
 
+app.post('/show', function(request, response) {
+    MongoClient.connect(url, function(err, client){
+        if(err) {
+            throw err;
+        }
+    
+        var db = client.db('express');
+        
+        db.collection('user').find().toArray(function(err, result){
+            if(err) {
+                throw err;
+            }
+    
+            response.send(result);
+        });
+
+        console.log('Users list is now visible on UI.');
+    });
+});
+
+app.patch('/', function(request, response) {
+    MongoClient.connect(url, function(err, client){
+        if(err) {
+            throw err;
+        }
+    
+        var db = client.db('express');
+        
+        db.collection('user').save(request.body);
+
+        console.log('User ' + request.body._id + ' has been updated');
+    }); 
+});
+
+function addTwoNumbers(x, y) {
+    return x + y;
+  }
+  console.log(addTwoNumbers(5, 1));
+
 app.listen(3000, ()=>console.log('express server has been started at port 3000'));
